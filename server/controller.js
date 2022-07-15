87e5b57b14ece46fa28420fe4c7b9dd15fba19a1
Trevor Draney <trevbot46@gmail.com>
2022-07-15 13:20:22 -0600

    Last Commit

---
+++ server/controller.js
@@ -0,0 +1,289 @@
+require('dotenv').config();
+
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize(process.env.CONNECTION_STRING, {
+    dialect: 'postgres',
+    dialectOptions: {
+        ssl: {
+            rejectUnauthorized: false
+        }
+    }
+})
+
+
+
+module.exports = {
+    seed: (req, res) => {
+        sequelize.query(`
+            DROP TABLE IF EXISTS cities;
+            DROP TABLE IF EXISTS countries;
+
+            CREATE TABLE countries (
+                country_id SERIAL PRIMARY KEY, 
+                name VARCHAR
+            );
+
+            CREATE TABLE cities (
+                city_id SERIAL PRIMARY KEY,
+                name VARCHAR,
+                rating INTEGER,
+                country_id INTEGER NOT NULL REFERENCES countries(country_id)
+            );
+
+            INSERT INTO countries (name)
+            VALUES ('Afghanistan'),
+            ('Albania'),
+            ('Algeria'),
+            ('Andorra'),
+            ('Angola'),
+            ('Antigua and Barbuda'),
+            ('Argentina'),
+            ('Armenia'),
+            ('Australia'),
+            ('Austria'),
+            ('Azerbaijan'),
+            ('Bahamas'),
+            ('Bahrain'),
+            ('Bangladesh'),
+            ('Barbados'),
+            ('Belarus'),
+            ('Belgium'),
+            ('Belize'),
+            ('Benin'),
+            ('Bhutan'),
+            ('Bolivia'),
+            ('Bosnia and Herzegovina'),
+            ('Botswana'),
+            ('Brazil'),
+            ('Brunei'),
+            ('Bulgaria'),
+            ('Burkina Faso'),
+            ('Burundi'),
+            ('Côte d''Ivoire'),
+            ('Cabo Verde'),
+            ('Cambodia'),
+            ('Cameroon'),
+            ('Canada'),
+            ('Central African Republic'),
+            ('Chad'),
+            ('Chile'),
+            ('China'),
+            ('Colombia'),
+            ('Comoros'),
+            ('Congo'),
+            ('Costa Rica'),
+            ('Croatia'),
+            ('Cuba'),
+            ('Cyprus'),
+            ('Czech Republic'),
+            ('Democratic Republic of the Congo'),
+            ('Denmark'),
+            ('Djibouti'),
+            ('Dominica'),
+            ('Dominican Republic'),
+            ('Ecuador'),
+            ('Egypt'),
+            ('El Salvador'),
+            ('Equatorial Guinea'),
+            ('Eritrea'),
+            ('Estonia'),
+            ('Eswatini'),
+            ('Ethiopia'),
+            ('Fiji'),
+            ('Finland'),
+            ('France'),
+            ('Gabon'),
+            ('Gambia'),
+            ('Georgia'),
+            ('Germany'),
+            ('Ghana'),
+            ('Greece'),
+            ('Grenada'),
+            ('Guatemala'),
+            ('Guinea'),
+            ('Guinea-Bissau'),
+            ('Guyana'),
+            ('Haiti'),
+            ('Holy See'),
+            ('Honduras'),
+            ('Hungary'),
+            ('Iceland'),
+            ('India'),
+            ('Indonesia'),
+            ('Iran'),
+            ('Iraq'),
+            ('Ireland'),
+            ('Israel'),
+            ('Italy'),
+            ('Jamaica'),
+            ('Japan'),
+            ('Jordan'),
+            ('Kazakhstan'),
+            ('Kenya'),
+            ('Kiribati'),
+            ('Kuwait'),
+            ('Kyrgyzstan'),
+            ('Laos'),
+            ('Latvia'),
+            ('Lebanon'),
+            ('Lesotho'),
+            ('Liberia'),
+            ('Libya'),
+            ('Liechtenstein'),
+            ('Lithuania'),
+            ('Luxembourg'),
+            ('Madagascar'),
+            ('Malawi'),
+            ('Malaysia'),
+            ('Maldives'),
+            ('Mali'),
+            ('Malta'),
+            ('Marshall Islands'),
+            ('Mauritania'),
+            ('Mauritius'),
+            ('Mexico'),
+            ('Micronesia'),
+            ('Moldova'),
+            ('Monaco'),
+            ('Mongolia'),
+            ('Montenegro'),
+            ('Morocco'),
+            ('Mozambique'),
+            ('Myanmar'),
+            ('Namibia'),
+            ('Nauru'),
+            ('Nepal'),
+            ('Netherlands'),
+            ('New Zealand'),
+            ('Nicaragua'),
+            ('Niger'),
+            ('Nigeria'),
+            ('North Korea'),
+            ('North Macedonia'),
+            ('Norway'),
+            ('Oman'),
+            ('Pakistan'),
+            ('Palau'),
+            ('Palestine State'),
+            ('Panama'),
+            ('Papua New Guinea'),
+            ('Paraguay'),
+            ('Peru'),
+            ('Philippines'),
+            ('Poland'),
+            ('Portugal'),
+            ('Qatar'),
+            ('Romania'),
+            ('Russia'),
+            ('Rwanda'),
+            ('Saint Kitts and Nevis'),
+            ('Saint Lucia'),
+            ('Saint Vincent and the Grenadines'),
+            ('Samoa'),
+            ('San Marino'),
+            ('Sao Tome and Principe'),
+            ('Saudi Arabia'),
+            ('Senegal'),
+            ('Serbia'),
+            ('Seychelles'),
+            ('Sierra Leone'),
+            ('Singapore'),
+            ('Slovakia'),
+            ('Slovenia'),
+            ('Solomon Islands'),
+            ('Somalia'),
+            ('South Africa'),
+            ('South Korea'),
+            ('South Sudan'),
+            ('Spain'),
+            ('Sri Lanka'),
+            ('Sudan'),
+            ('Suriname'),
+            ('Sweden'),
+            ('Switzerland'),
+            ('Syria'),
+            ('Tajikistan'),
+            ('Tanzania'),
+            ('Thailand'),
+            ('Timor-Leste'),
+            ('Togo'),
+            ('Tonga'),
+            ('Trinidad and Tobago'),
+            ('Tunisia'),
+            ('Turkey'),
+            ('Turkmenistan'),
+            ('Tuvalu'),
+            ('Uganda'),
+            ('Ukraine'),
+            ('United Arab Emirates'),
+            ('United Kingdom'),
+            ('United States of America'),
+            ('Uruguay'),
+            ('Uzbekistan'),
+            ('Vanuatu'),
+            ('Venezuela'),
+            ('Vietnam'),
+            ('Yemen'),
+            ('Zambia'),
+            ('Zimbabwe');
+
+        INSERT INTO cities (name, rating, country_id)
+        VALUES ('Paris', 5, 61),
+            ('Sandy', 5, 61),
+            ('Draper', 4, 61),
+            ('Lehi', 3, 61),
+            ('American Fork', 4, 61),
+            ('Salt Lake City', 2, 61),
+            ('Spanish Fork', 5, 61),
+            ('Orem', 4, 61);
+        `).then(() => {
+            console.log('DB seeded!')
+            res.sendStatus(200)
+        }).catch(err => console.log('error seeding DB', err))
+    },
+
+    getCountries: (req, res) => {
+        sequelize.query(`
+        SELECT *
+        FROM countries
+        `)
+        .then(dbRes => res.status(200).send(dbRes[0]))
+        .catch(err => console.log(err))
+    },
+
+    createCity: (req, res) => {
+        let {name, rating, countryId} = req.body;
+        sequelize.query(`
+        INSERT INTO cities (name, rating, country_id)
+        VALUES ('${name}', '${rating}', '${countryId}');
+        `)
+        .then(dbRes => res.status(200).send(dbRes[0]))
+        .catch(err => console.log(err))
+    },
+
+    getCities: (req, res) => {
+        sequelize.query(`
+        SELECT ci.city_id, ci.name, ci.rating, co.name, co.country_id
+        FROM cities AS ci
+        JOIN countries AS co
+        ON ci.country_id = co.country_id
+        ORDER BY ci.rating desc;
+        `)
+        .then(dbRes => {
+            console.log(dbRes[0])
+            res.status(200).send(dbRes[0])})
+        .catch(err => console.log(err))
+    },
+
+    deleteCity: (req, res) => {
+        let {id} = req.params
+        sequelize.query(`
+        DELETE 
+        FROM cities
+        WHERE city_id = ${id}
+        `)
+        .then(dbRes => res.status(200).send(dbRes[0]))
+        .catch(err => console.log(err))
+    }
+}
